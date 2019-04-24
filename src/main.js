767475195daf7ccd752d05969311da0212939039
Paul Bennings <paul.bennings@codeimmersives.com>
2019-04-24 16:40:05 -0400

    printing out items

--- src/main.js
+++ src/main.js
@@ -1,52 +1,60 @@
 window.onload = init;
 
 function init() {
-    document.querySelector('#get').addEventListener('click', getTodos);
-    document.querySelector('#post').addEventListener('click', postTodo);
-    document.querySelector('#put').addEventListener('click', updateThirdTodo);
+  document.querySelector("#get").addEventListener("click", getTodos);
+  document.querySelector("#post").addEventListener("click", postTodo);
+  document.querySelector("#put").addEventListener("click", updateThirdTodo);
 }
 
 function getTodos(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:3000/todos');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = handleData;
-    xhr.send();
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://localhost:3000/todos");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = handleData;
+  xhr.send();
 }
 
 function postTodo(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const newTodo = {
-        text: "Profit",
-        completed: false
-    };
-    
-    const jsonnedTodo = JSON.stringify(newTodo);
-    
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/todos');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = handleData;
-    xhr.send(jsonnedTodo);
+  const newTodo = {
+    text: "Profit",
+    completed: false
+  };
+
+  const jsonnedTodo = JSON.stringify(newTodo);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:3000/todos");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = handleData;
+  xhr.send(jsonnedTodo);
 }
 
 function updateThirdTodo(event) {
-    const updatedTodo = {
-        text: '????',
-        completed: true
-    };
-    const jsonnedTodo = JSON.stringify(updatedTodo);
-    
-    const xhr = new XMLHttpRequest();
-    xhr.open('PUT', 'http://localhost:3000/todos/3');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = handleData;
-    xhr.send(jsonnedTodo);
+  const updatedTodo = {
+    text: "????",
+    completed: true
+  };
+  const jsonnedTodo = JSON.stringify(updatedTodo);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("PUT", "http://localhost:3000/todos/3");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = handleData;
+  xhr.send(jsonnedTodo);
 }
 
 function handleData(event) {
-    console.log(event.target.responseText);
+  event.preventDefault();
+  const data = JSON.parse(event.target.responseText);
+
+  for (const i = 0; i < handleData; i++);
+  const list = document.querySelector("#list");
+  const newLi = document.createElement("li");
+  newLi.innerText = data[0].text;
+  list.appendChild(newLi);
+  console.log(data);
 }
